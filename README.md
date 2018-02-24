ed4e3f0f5f7f5558ee7dde61f0224d5c7bfc5c16
Matt <mb@issuu.com>
2018-02-23 19:19:54 -0800

    Initial commit ✨

---
+++ README.md
@@ -0,0 +1,29 @@
+# Kodi Episode Deduplicator
+
+This script scans a Kodi video library for duplicate files of the same episode and marks all but one of them as watched.
+
+It can be started by visiting a simple URL.
+
+## Usage
+
+1. Run `npm install`
+1. Run `npm start`
+1. Visit [`http://localhost:3000`](http://localhost:3000) to just see what duplicates you have
+1. Visit [`http://localhost:3000?mark_watched=true`](http://localhost:3000?mark_watched=true) to mark all but one duplicate per episode as watched
+
+The script will prefer to keep episodes with subtitles and higher resolutions unwatched.
+
+![](https://media.giphy.com/media/eirkO1X9Tma88/giphy.gif)
+
+## Configuration
+By default, the script will be available locally at port `3000`. You can change that by setting the `PORT` environment variable like so `PORT=8000 npm start`.
+
+To tell the script what host your Kodi instance is running under, set the `KODI_HOST` environment variable like so `KODI_HOST=192.168.1.140 npm start`.
+
+The script will connect to Kodi at websocket port `9090`, but if you want it to use a different port, you can set the `KODI_WS_PORT` environment variable like so `KODI_WS_PORT=9191 npm start`.
+
+All environment variables are combinable.
+
+## Development
+
+Run `npm run dev` to automatically restart your server after every file change. ✨
---
+++ index.js
@@ -0,0 +1,127 @@
+const kodi = require('kodi-ws');
+const app = require('express')();
+const port = process.env.PORT || 3000;
+const kodiHost = process.env.KODI_HOST || 'kodi.my.home';
+const kodiWebsocketPort = process.env.KODI_WS_PORT || 9090;
+
+function getWorstVideo(v1, v2) {
+    const v1worst = -1;
+    const v2worst = 1;
+
+    const v1v = v1.streamdetails.video[0];
+    const v2v = v2.streamdetails.video[0];
+
+    if (v1.streamdetails.subtitle.length > v2.streamdetails.subtitle.length) {
+        return v2worst;
+    }
+    if (v1.streamdetails.subtitle.length < v2.streamdetails.subtitle.length) {
+        return v1worst;
+    }
+
+    if (!v2v) return v2worst;
+    if (!v1v) return v1worst;
+
+    if (v1v.height > v2v.height) {
+        return v2worst;
+    }
+    if (v1v.height < v2v.height) {
+        return v1worst;
+    }
+
+    if (v1v.duration > v2v.duration) {
+        return v2worst;
+    }
+    if (v1v.duration < v2v.duration) {
+        return v1worst;
+    }
+
+    return 0;
+}
+
+function getKodiConnection() {
+    return kodi(kodiHost, kodiWebsocketPort);
+}
+
+function getDeletableDuplicates() {
+    return getKodiConnection().then(kodi => {
+        return (
+            kodi.VideoLibrary.GetEpisodes({
+                properties: [
+                    'showtitle',
+                    'playcount',
+                    'tvshowid',
+                    'uniqueid',
+                    'streamdetails',
+                    'episode',
+                    'season',
+                    'file',
+                ],
+                filter: { field: 'playcount', operator: 'is', value: '0' },
+            })
+                .then(r => r.episodes)
+                // build map of unique episodes with array of files
+                .then(episodes =>
+                    episodes.reduce((acc, episode) => {
+                        const key = `${episode.tvshowid}-${episode.season}-${episode.episode}`;
+                        const list = acc[key] || [];
+                        episode.key = key;
+                        list.push(episode);
+                        acc[key] = list;
+                        return acc;
+                    }, {})
+                )
+                // remove episodes that don't have duplicates
+                .then(consolidatedEpisodes =>
+                    Object.values(consolidatedEpisodes).filter(episodeBatch => episodeBatch.length > 1)
+                )
+                // remove the file that we want to keep/that shouldn't get "deleted"
+                .then(episodeBatches =>
+                    episodeBatches.map(episodeBatch => episodeBatch.sort(getWorstVideo).slice(0, -1))
+                )
+                // turn that map back into an array of deletable episodes
+                .then(episodeBatches =>
+                    episodeBatches.reduce((acc, episodeBatch) => {
+                        return acc.concat(episodeBatch);
+                    }, [])
+                )
+        );
+    });
+}
+
+function setEpisodesWatched(episodes) {
+    return getKodiConnection().then(kodi => {
+        episodes.forEach(episode => {
+            console.log('  ⏹  Marking as watched:', episode.showtitle, episode.label);
+            kodi.VideoLibrary.SetEpisodeDetails({ episodeid: episode.episodeid, playcount: 1 })
+                .then(() => {
+                    console.log('  ✅  Marked as watched:', episode.showtitle, episode.label, episode.uniqueid);
+                })
+                .catch(e => {
+                    console.error('🛑  Error during', episode.showtitle, episode.label, e);
+                });
+        });
+        return episodes;
+    });
+}
+app.get('/', (req, res) => {
+    getDeletableDuplicates()
+        .then(episodes => {
+            if (req.query.mark_watched === 'true') {
+                return setEpisodesWatched(episodes);
+            }
+            return episodes;
+        })
+        .then(episodes => {
+            res.json(episodes);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(503).json(error);
+        });
+});
+
+app.listen(port, () => {
+    console.log(`👂  Listening on port ${port}`);
+    console.log(`🌍  Visit http://localhost:${port} to see duplicate episodes`);
+    console.log(`✅  Visit http://localhost:${port}?mark_watched=true to mark duplicate episodes as watched`);
+});
